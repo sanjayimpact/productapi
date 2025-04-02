425793e0b3edd68493cabe98de0ed6d4f578280d
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2025-04-02 10:28:46 +0530

    sda

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(cors());
 
 
 app.use('/api',userRouter)
-
+app.use('/',(req,res)=>{
+    res.send("hello world")
+})
 app.listen(port,()=>{
     connectDb();
     console.log(`server is running on port ${port}`)
