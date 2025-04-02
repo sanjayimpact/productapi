bb0d1ec84ebf76636908dfedb13e9b5e6a5999e1
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2025-04-02 11:40:09 +0530

    update

--- controllers/getname.js
+++ controllers/getname.js
@@ -1,3 +1,5 @@
+import connectDb from "../db.js"
+
 export const getname = (req,res)=>{
-    res.json({message:"new respond",isSuccess:true})
+    res.json({message:"new respond",isSuccess:true,data:connectDb})
 }
