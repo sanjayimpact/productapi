2e97250458b62cd2d9f4930dde3f5e210c982d51
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2025-04-11 18:11:11 +0530

    update

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async()=>{
     try{
-         await mongoose.connect("mongodb+srv://admin:admin@cluster0.lcm6m.mongodb.net/Ecart")
+         await mongoose.connect("mongodb+srv://admin:admin@cluster0.lcm6m.mongodb.net/vapetocart")
          console.log("database successfully connected")
     }catch(err){}
 }
